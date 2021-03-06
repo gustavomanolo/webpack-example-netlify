ff31f7832c7e0ac48aa8123443567fc55c99d770
Gustavo Manolo <gustavomanolo@gmail.com>
2021-03-05 18:56:59 -0600

    init commit
    Deploy to Netlify

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Template from './templates/Template.js';
+import '@styles/main.css'
+import '@styles/vars.styl'
+
+(async function App() {
+  const main = null || document.getElementById('main');
+  main.innerHTML = await Template();
+})();
+
+console.log('-> hello3')
